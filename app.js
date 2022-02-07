d3f5de7b2b241d976e96a3bc99d85e9d2c8f9708
unknown <saadathadunniade@gmail.com>
2022-02-07 13:51:20 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+// window.addEventListener("scroll", function () {
+//     let nav = document.querySelector(".header");
+//     let theWindow = window.scrollY > 0;
+//     nav.classList.toggle("scrolling", theWindow);
+// })
